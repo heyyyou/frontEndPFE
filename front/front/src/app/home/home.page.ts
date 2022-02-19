d56e2507426781d616600566efc74d06d55d7557
heyyyou <chellymariem01@gmail.com>
2022-02-19 03:04:22 +0100

    feature/design-home

--- front/front/src/app/home/home.page.ts
+++ front/front/src/app/home/home.page.ts
@@ -4,6 +4,8 @@ import { SwiperOptions, Pagination } from 'swiper';
 import { ActionSheetController } from '@ionic/angular';
 import { ModalImageComponent } from '../modal-image/modal-image.component';
 import { UserPhoto, PhotoService } from '../services/photo.service';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -51,7 +53,15 @@ export class HomePage {
     "assets/4.jpg",
     "assets/empty.jpg"
   ]
-  constructor(private modalCtrl: ModalController, public photoService: PhotoService, public actionSheetController: ActionSheetController) { }
+  constructor(private modalCtrl: ModalController, public photoService: PhotoService, public actionSheetController: ActionSheetController, public alertController: AlertController, public toastController: ToastController) { }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: '5 élèments maximum,merci',
+      duration: 2000
+    });
+    toast.present();
+  }
+
   async ngOnInit() {
     await this.photoService.loadSaved();
   }
@@ -96,6 +106,5 @@ export class HomePage {
 
     }
   }
+
 }
-
-
